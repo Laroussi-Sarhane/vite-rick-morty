1d7c5888d0902955e5deded1ec49b17297f03de4
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-25 23:07:43 +0100

    add name changes

--- src/components/partials/card.vue
+++ src/components/partials/card.vue
@@ -14,8 +14,9 @@ export default {
   <div class="card mb-4 " style="width: 18rem;">
   <img :src="image" class="card-img-top" :alt="name">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">{{ name }}</h5>
+    <p class="card-text">{{ gender }}</p>
+    <p class="card-text">{{ status }}</p>
    
   </div>
 </div>
